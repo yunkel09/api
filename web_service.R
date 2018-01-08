c3d2d1ed4bfab5a396d1539789dfbb7b1df130b0
William Chavarria <wchavarria@tigo.com.gt>
2018-01-07 21:55:33 -0600

    segundo

--- web_service.R
+++ web_service.R
@@ -8,19 +8,19 @@
       SOAPAction = "http://www.banguat.gob.gt/variables/ws/TipoCambioDia"
     )
     
-    body = "<?xml version='1.0' encoding='utf-8'?> <soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'> <soap:Body> <TipoCambioDia xmlns='http://www.banguat.gob.gt/variables/ws/' /> </soap:Body> </soap:Envelope"
+    # version SOAP 1.1
+body_1.1 = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><TipoCambioDia xmlns="http://www.banguat.gob.gt/variables/ws/"></soap:Body></soap:Envelope>'
+    
+    
     
     reader <- basicTextGatherer()
     header <- basicHeaderGatherer()
     handle <- getCurlHandle()
 
     curlPerform(
-      url            = "http://www.banguat.gob.gt/variables/ws/TipoCambioDia/",
+      url            = "http://www.banguat.gob.gt/variables/ws/TipoCambioDia.asmx",
       httpheader     = headerfields,
-      postfields     = body,
-      writefunction  = reader$update, 
-      headerfunction = header$update, 
-      curl           = handle,
-      verbose        = TRUE)
+      postfields     = body_1.1)
+    
 
 header$value()
